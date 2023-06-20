61fbe4128fe751a89111fbfb6c0282e3682327d7
KalebHedrick <destroycows@gmail.com>
2023-06-20 17:02:12 -0400

    image upload

---
+++ fetchData.js
@@ -0,0 +1,96 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export const FetchAllWeaponsData = async () => { //Return a promise with all weapons
+    try {
+        const response = await fetch(
+          'https://valorant-api.com/v1/weapons',{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+        }
+        );
+        if (response.status == 200) { 
+          
+          console.log("ALL WEAPON DATA FROM API RETRIEVED");
+          return await response.json();
+        }
+        else {
+          console.error("Fetch error occured, retrying");
+        FetchAllWeaponsData();
+        }
+      } catch (error) {
+        console.error("Fetch error occured, retrying");
+        FetchAllWeaponsData();
+      }
+};
+
+export const FetchWeaponbyUUID = async (WUUID) => { //Return a promise with all weapon data
+    try {
+        const response = await fetch(
+          'https://valorant-api.com/v1/weapons/' + WUUID,
+            {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+        }
+        );
+        if (response.status == 200) { 
+          
+          console.log("Weapon with UUID" + WUUID + "Retreived")
+          return await response.json();
+        }
+        else {
+          console.error("Fetch error occured, retrying");
+          FetchWeaponbyUUID(WUUID);
+        }
+      } catch (error) {
+        console.error("Fetch error occured, retrying");
+        FetchWeaponbyUUID(WUUID);
+      }
+};
+export const FetchWeaponSkinbyUUID = async (WUUID) => { //Return a promise with all data from skin
+  try {
+      const response = await fetch(
+        'https://valorant-api.com/v1/weapons/skins/' + WUUID,
+          {
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          method: 'GET',
+      }
+      );
+      if (response.status == 200) { 
+        
+        console.log("Weapon with UUID" + WUUID + "Retreived")
+        return await response.json();
+      }
+      else {
+        console.error("Fetch error occured, retrying");
+        FetchWeaponSkinbyUUID(WUUID);
+      }
+    } catch (error) {
+      console.error("Fetch error occured, retrying");
+      FetchWeaponSkinbyUUID(WUUID);
+    }
+};
+
+export async function save(key, value) { //saves data to storage
+
+ return await AsyncStorage.setItem(key,JSON.stringify(value));
+}
+
+export async function getValueFor(key) { //returns data from storage
+  let result = await AsyncStorage.getItem(key);
+  if (typeof result === 'string' || result instanceof String) {
+    console.log("data with key: " + key + " retrieved from local storage");
+    return await result;
+  }
+  else {
+    
+    getValueFor(key);
+  }
+}
